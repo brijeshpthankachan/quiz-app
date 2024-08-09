42e8d08e4c6c4d6602e7efe46c8eedf6fabf5fe1
brijesh p thankachan <brijeshpthankachan@gmail.com>
2024-08-09 21:38:59 +0530

    corrected prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export default prisma;
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
